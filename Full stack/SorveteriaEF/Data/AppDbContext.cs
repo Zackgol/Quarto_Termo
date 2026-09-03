a578e28a2f98f0cf6606422b0f78292fe1e61339
Zackgol <zaqueulittleboy@gmail.com>
2026-09-02 21:54:01 -0300

    Aprendizado do EntityFramework

---
+++ Full stack/SorveteriaEF/Data/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using System.Data.Entity;
+using SorveteriaEF.Models;
+
+namespace SorveteriaEF.Data
+{
+    public class AppDbContext : DbContext
+    {
+        // O nome passado em base("name=...") referencia a chave no App.config
+        public AppDbContext() : base("name=ConexaoSQLServer")
+        {
+        }
+
+        public DbSet<Sorvete> Sorvetes { get; set; }
+    }
+}
---
+++ Full stack/SorveteriaEF/Migrations/202609030030501_Correcao.cs
@@ -0,0 +1,18 @@
+﻿namespace SorveteriaEF.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class Correcao : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.Sorvetes", "Tamanho", c => c.String());
+        }
+        
+        public override void Down()
+        {
+            DropColumn("dbo.Sorvetes", "Tamanho");
+        }
+    }
+}
